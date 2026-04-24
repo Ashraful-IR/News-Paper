d7ffe246c780c5378b3504dd1ecd76e779d15162
Ashraful-IR <rifo0118@gmail.com>
2026-04-25 01:51:32 +0600

    Initial project setup done

---
+++ src/Component/Shared/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavbarPage = () => {
+    return (
+        <div>
+            <h1>This is navebar</h1>
+        </div>
+    );
+};
+
+export default NavbarPage;
